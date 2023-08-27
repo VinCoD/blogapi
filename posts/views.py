ba31abb472d4cbd28c566a552180b5b08af87b56
VinCoD <5895.2020@students.ku.ac.ke>
2023-08-27 18:13:32 +0300

    Added viewsets and routers

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,27 @@
-from rest_framework import generics
+from django.contrib.auth import get_user_model # new
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
+
 from .models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 
 
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)  # new
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):  # new
+    permission_classes = [IsAdminUser]
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+"""
 class PostList(generics.ListCreateAPIView):
     permission_classes = (IsAuthorOrReadOnly,)  # new
     queryset = Post.objects.all()
@@ -16,4 +32,17 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthorOrReadOnly,)  # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):  # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):  # new
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    """
+
+
 
